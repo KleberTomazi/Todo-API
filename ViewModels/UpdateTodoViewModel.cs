7ccbe3d23ad3c39280f56133aaa6bc1b90f4c888
Kleber Tomazi <kleberluiz9@gmail.com>
2021-10-18 17:52:54 -0300

    Adding some code

---
+++ ViewModels/UpdateTodoViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTodo.ViewModels
+{
+    public class UpdateTodoViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+    }
+}
